9bb1af1bb6008947a21a113835059686a3622bb0
Alice <wheresalice@users.noreply.github.com>
2018-08-17 16:08:13 +0100

    add some comments

--- main.go
+++ main.go
@@ -15,13 +15,16 @@ import (
 	"bytes"
 )
 
+// commandline parameters
 var opts struct {
 	Broker string `short:"b" long:"broker" description:"kafka broker to connect to" required:"true"`
 	Topic string `short:"t" long:"topic" description:"kafka topic to check" required:"true"`
 	Port int `short:"p" long:"port" description:"http port to listen on" default:"8080"`
 }
+
 type TimeStamp struct{}
 
+// process a message, returning a timestamp for every message
 func (TimeStamp) ProcessMessage(m types.Message) ([]types.Message, types.Response) {
 	// Create a copy of the original message
 	result := m.Copy()
@@ -39,9 +42,8 @@ func (TimeStamp) ProcessMessage(m types.Message) ([]types.Message, types.Respons
 	return []types.Message{result}, nil
 }
 
-
-
 func main() {
+	// parse the input parameters and panic horribly if we don't have the required ones
 	_, err := flags.Parse(&opts)
 	if err != nil {
 		panic(err)
@@ -49,35 +51,36 @@ func main() {
 
 	conf := stream.NewConfig()
 
+	// configure the input to be a simple kafka topic
 	conf.Input.Type = input.TypeKafka
 	conf.Input.Kafka.Addresses = []string{opts.Broker}
 	conf.Input.Kafka.Topic = opts.Topic
 
+	// configure the output to be an http server, mostly with the default paths
 	conf.Output.Type = output.TypeHTTPServer
 	address := fmt.Sprintf("0.0.0.0:%d", opts.Port)
 	conf.Output.HTTPServer.Address = address
 
+	// log out what we're doing, mostly to confirm we've read parameters correctly
 	log.Printf("connecting to: %v", conf.Input.Kafka.Addresses)
 	log.Printf("reading from topic: %v", conf.Input.Kafka.Topic)
 	log.Printf("listening on: %s", address)
 
+	// start the engines
 	s, err := stream.New(conf, stream.OptAddProcessors(func() (types.Processor, error) {
 		return TimeStamp{}, nil
 	}))
 	if err != nil {
 		panic(err)
 	}
-
 	defer s.Stop(time.Second)
 
+	// exit cleanly on ctrl-c
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
-
 	select {
 	case <-sigChan:
 		log.Println("Received SIGTERM, the service is closing")
 	}
-
-
 
 }
